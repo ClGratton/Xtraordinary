b56ac402c1f5da83a8a17519c41de0bf17163e25
ClGratton <cla20gra@gmail.com>
2026-08-16 20:58:19 +0200

    Fix Compose color animation import

--- app/src/main/java/com/xteink/companion/ui/components/ExpandingChoice.kt
+++ app/src/main/java/com/xteink/companion/ui/components/ExpandingChoice.kt
@@ -1,12 +1,12 @@
 package com.xteink.companion.ui.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
